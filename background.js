1551c2a3d7fd9f10b927019e3f59e23cd1d73715
Robert Talamantez <rtalamantez@localhost.localdomain>
2014-06-20 11:58:03 -0700

    sees seria

--- background.js
+++ background.js
@@ -8,11 +8,29 @@ chrome.app.runtime.onLaunched.addListener(function() {
 
 });
 
-var onGetDevices = function(ports) {
+
+var onConnect = function(connectionInfo) {
+    console.log('in onConnect');
+
+    console.dir(this);
+    console.dir(_this);
+    // The serial port has been opened. Save its id to use later.
+    console.dir(connectionInfo.connectionId);
+    //_this.connectionId = connectionInfo.connectionId;
+    // Do whatever you need to do with the opened port.
+
+}
+// Connect to the serial port /dev/ttyACM0
+chrome.serial.connect("/dev/ttyACM0", {
+    bitrate: 115200
+}, onConnect);
+
+//chrome.serial.getDevices(onGetDevices);
+
+/*var onGetDevices = function(ports) {
     console.log('in onGetDevices');
     for (var i = 0; i < ports.length; i++) {
         console.log(ports[i].path);
     }
 }
-
-chrome.serial.getDevices(onGetDevices);
+*/
