d4cfa580f85b6f43df7584e4dfa0b4a8c5936f70
Robert Talamantez <rtalamantez@localhost.localdomain>
2014-06-20 09:41:49 -0700

    serial data

--- background.js
+++ background.js
@@ -1,65 +1,18 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html', {
-    'bounds': {
-      'width': 400,
-      'height': 500
-    }
-  });
+    chrome.app.window.create('window.html', {
+        'bounds': {
+            'width': 400,
+            'height': 500
+        }
+    });
 
 });
 
-var transferInfo = {
-  "direction": "in",
-  "endpoint": 1,
-  "length": 2560
-};
-
-var isoTransferInfo = {
-  "transferInfo": transferInfo,
-  "packets": 20,
-  "packetLength": 128
-};
-
-var onDeviceFound = function (devices) {
-  console.log("onDeviceFound", devices);
-  this.devices=devices;
-  if (devices) {
-    if (devices.length > 0) {
-      console.log("Device(s) found: "+devices.length);
-      console.log(devices);
-      var device = devices[0];
-      window.device = device;
-      chrome.usb.openDevice(device, onOpenDevice);
-    } else {
-      console.log("Device could not be found");
+var onGetDevices = function(ports) {
+    console.log('in onGetDevices');
+    for (var i = 0; i < ports.length; i++) {
+        console.log(ports[i].path);
     }
-  } else {
-    console.log("Permission denied.");
-  }
 }
 
-
-var onOpenDevice= function(connectionHandle) {
-  console.log('in onOpenDevice');
-
-  if (connectionHandle) {
-   // console.dir(connectionHandle);
-   console.log("Device opened.");
-    // transferData(connectionHandle)
-                                // device           setttings         cb
-    chrome.usb.isochronousTransfer(connectionHandle, isoTransferInfo,randomCallback);
-//console.dir(chrome.usb.isochronousTransfer(connectionHandle, isoTransferInfo));
-} else {
-  console.log("Device failed to open.");
-}
-}
-var randomCallback=function(isoTransferInfo){
-  console.dir(isoTransferInfo);
-  console.dir(ArrayBuffer(isoTransferInfo));
-}
-
-console.dir(chrome.usb);
-var vendorId = 9025;
-var productId = 67;
-chrome.usb.getDevices({"vendorId": vendorId, "productId": productId}, onDeviceFound);
-
+chrome.serial.getDevices(onGetDevices);
--- getSerial.js
+++ getSerial.js
@@ -1,18 +1,36 @@
-// chrome.usb.getDevices(enumerateDevicesOptions, callback);
+console.log('HIHIHIHI');
+
 
 function onDeviceFound(devices) {
-  this.devices=devices;
-  if (devices) {
-    if (devices.length > 0) {
-      console.log("Device(s) found: "+devices.length);
-      console.log(devices);
+    console.log('in onDeviceFound');
+    this.devices = devices;
+    if (devices) {
+        if (devices.length > 0) {
+            console.log("Device(s) found: " + devices.length);
+            console.log(devices);
+        } else {
+            console.log("Device could not be found");
+        }
     } else {
-      console.log("Device could not be found");
+        console.log("Permission denied.");
     }
-  } else {
-    console.log("Permission denied.");
-  }
 }
+
+chrome.usb.getDevices({
+    "vendorId": vendorId,
+    "productId": productId
+}, onDeviceFound);
+
+var onGetDevices = function(ports) {
+    console.log('in onGetDevices');
+    for (var i = 0; i < ports.length; i++) {
+        console.log(ports[i].path);
+    }
+}
+
 var vendorId = 2341;
 var productId = 0043;
-chrome.usb.getDevices({"vendorId": vendorId, "productId": productId}, onDeviceFound);
+
+chrome.usb.getDevices(enumerateDevicesOptions, callback);
+
+chrome.serial.getDevices(onGetDevices);
